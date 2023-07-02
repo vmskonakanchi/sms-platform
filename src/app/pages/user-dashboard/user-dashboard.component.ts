7bc185d9e336d63edb5fd45bd19ec46d624933ef
Gowtham-Ghost <120414813+Gowtham-Ghost@users.noreply.github.com>
2023-07-02 19:04:08 +0530

    loginpage,homepage

--- src/app/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.css'],
 })
-export default class UserDashboardComponent {}
+export default class UserDashboardComponent implements OnInit {
+  ngOnInit(): void {}
+
+  toogleClass() {
+    let sidebar = <HTMLElement>document.querySelector('.sidebar');
+    let closeBtn = <HTMLElement>document.querySelector('#btn');
+    let logo = <HTMLElement>document.querySelector('#logo');
+    sidebar?.classList.toggle('open');
+    if (sidebar?.classList.contains('open')) {
+      closeBtn?.classList.add('bx-menu');
+      logo?.classList.replace('bx-menu', 'bxs-message-dots'); //replacing the iocns class
+    } else {
+      closeBtn?.classList.remove('bx-menu');
+      logo?.classList.replace('bxs-message-dots', 'bx-menu'); //replacing the iocns class
+    }
+  }
+}
